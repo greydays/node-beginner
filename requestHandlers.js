34bae718513c0e5cbb6d67efbbee0e861f38e0bb
Ryan Flaherty <rcflaherty@gmail.com>
2016-03-03 12:39:16 -0800

    first commit, docs added

---
+++ requestHandlers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function start() {
+  console.log('Request handler "start" was called.');
+}
+
+function upload() {
+  console.log('Request handler "upload" was called.');
+}
+
+exports.start = start;
+exports.upload = upload;
+
---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var http = require('http');
+var url = require('url');
+
+function start(route, handle){
+  function onRequest(request, response){
+
+    // kills annoying favicon console messages https://gist.github.com/kentbrew/763822
+    if (request.url === '/favicon.ico') {
+      response.writeHead(200, {'Content-Type': 'image/x-icon'} );
+      response.end();
+      return;
+    }
+
+    var pathname = url.parse(request.url).pathname;
+    console.log('Request for ' + pathname + 'received.');
+
+    route(handle, pathname);
+
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World');
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started on port 8888');
+}
+
+exports.start = start;
