f191a211803a29f8dc9f236dcb1d73a5a90eaa34
Ryan Flaherty <rcflaherty@gmail.com>
2016-03-04 21:28:04 -0800

    2nd iteration, tuturorial nearly complete

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,46 @@
 'use strict';
 
-function start() {
+var querystring = require('querystring');
+var fs = require('fs');
+
+function start(response, postData) {
   console.log('Request handler "start" was called.');
+
+  var body =
+    '<html>' +
+    '<head>' +
+    '<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />' +
+    '</head>' +
+    '<body>' +
+    '<form action="/upload" method="post">' +
+    '<textarea name="text" cols="60" rows="20">' +
+    '</textarea>' +
+    '<input type="submit" value="Submit text">' +
+    '</form>' +
+    '</body>' +
+    '</html>';
+
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write(body);
+  response.end();
 }
 
-function upload() {
+function upload(response, postData) {
   console.log('Request handler "upload" was called.');
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('You have sent the text: ' +
+  querystring.parse(postData).text);
+  response.end();
+}
+
+function show(response) {
+  console.log('Request handler "show" was called.');
+  response.writeHead(200,  {'Content-Type': 'img/png'});
+  fs.createReadStream('/tmp/test.png').pipe(response);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
+
 
--- server.js
+++ server.js
@@ -13,14 +13,22 @@ function start(route, handle){
       return;
     }
 
+    var postData = '';
     var pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + 'received.');
 
-    route(handle, pathname);
+    request.setEndcoding('utf8');
 
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World');
-    response.end();
+    request.addListener('data', function(postDataChunk) {
+      postData += postDataChunk;
+      console.log('Received POST data chunk "' + postDataChunk + '."');
+    });
+
+    request.addListener('end', function(){
+      route(handle, pathname, response, postData);
+    });
+
+    route(handle, pathname, response);
   }
 
   http.createServer(onRequest).listen(8888);
@@ -28,3 +36,4 @@ function start(route, handle){
 }
 
 exports.start = start;
+
